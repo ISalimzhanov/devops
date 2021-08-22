d36cd93b7db0c93ac46bf4c02805f870e96a84f9
IsSalimzhanov <Iskander.Salimzhanov@scan.com.ru>
2021-08-22 20:41:07 +0300

    license and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="Simple web app to show current time in Moscow",
+    version="0.0.1",
+    description="Simple web app to show current time in Moscow",
+    long_description=readme,
+    author="Iskander Salimzhanov",
+    url="https://github.com/ISalimzhanov/devops_lab1",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs")),
+)
